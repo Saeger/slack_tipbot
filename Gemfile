995fea4ca04bf4ebfcb9799bbabacad206deb092
jacob zlogar <jacob.zlogar@gmail.com>
2017-09-27 00:26:56 -0400

    update

--- Gemfile
+++ Gemfile
@@ -1,8 +1,9 @@
 ruby "2.3.1"
 source "https://rubygems.org"
 
+gem 'eventmachine'
+gem 'faye-websocket'
 gem "dotenv-rails"
-gem "sinatra"
-gem "sinatra-contrib"
+gem 'slack-ruby-client'
 gem "bitcoin-client"
 gem "rspec"
--- tipper.rb
+++ tipper.rb
@@ -1,25 +1,16 @@
-require 'sinatra'
-require "sinatra/json"
 require 'bitcoin-client'
 require './src/bitcoin_client_extensions.rb'
 require './src/command.rb'
+require 'dotenv/load'
 set :bind, '0.0.0.0'
 
-raise "Please set SLACK_API_TOKEN" if ENV['SLACK_API_TOKEN'].nil?
+puts ENV['SLACK_API_TOKEN']
 
-post "/tip" do
-  puts params
-  raise "NOP" unless params['token'] == ENV['SLACK_API_TOKEN']
+raise "Invalid slack token" if ENV['SLACK_API_TOKEN'].nil?
 
-  begin
-    command = Command.new(params)
-    command.perform
-    json command.result
-  rescue Exception => ex
-    json text: "error: #{ex.message}", icon_emoji: ":japanese_goblin:"
-  end
-end
-
-get "/" do
-  "it works!"
+begin
+  command = Command.new(params)
+  command.perform
+rescue Exception => ex
+  # error
 end
