b3154538b80018a6d0eec1e7d4911dc36465ebad
Márcio Saeger <marcio@marciosaeger.com.br>
2017-10-03 10:12:19 -0300

    Fix minimum amount to withdraw. 0.0001 are needed for fee.

--- command.rb
+++ command.rb
@@ -108,7 +108,7 @@ class Command
     @amount = amount.to_i
     randomize_amount if (@amount == "random")
     
-    raise @coin_config_module::TOO_POOR_TEXT unless available_balance >= @amount + 1
+    raise @coin_config_module::TOO_POOR_TEXT unless available_balance >= @amount + 0.0001
     raise @coin_config_module::NO_PURPOSE_LOWER_BOUND_TEXT if @amount < @coin_config_module::NO_PURPOSE_LOWER_BOUND
   end
 
